c61a3fd1c55d597e562428d1e8a5c547aa1aa264
Your Name <you@example.com>
2024-07-18 15:18:30 +0430

    feat(customer): make all components and responsive

---
+++ src/app/customers/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Customers from '@/components/customers/Customers';
+
+export default function Customerpage() {
+  return (
+    <div className='min-h-screen bg-[#010101]'>
+      <Customers />
+      <Customers />
+    </div>
+  );
+}
