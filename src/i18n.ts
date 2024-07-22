ad694c3a391899bea6415bb118c1c4c5962f3155
SayeedMahdi <sayeedmahdimousavi789@gmail.com>
2024-07-22 14:00:21 +0430

    fix(file): change that should work

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 
-import { locales } from './config/locales';
+// Can be imported from a shared config
+const locales = ['en', 'de'];
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale)) {
-    notFound();
-  }
+  if (!locales.includes(locale as any)) notFound();
 
   return {
     messages: (await import(`../messages/${locale}.json`)).default,
--- src/middleware.ts
+++ src/middleware.ts
@@ -12,5 +12,5 @@ export default createMiddleware({
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(en|da|pa)/:path*'],
+  matcher: ['/', '/(en|da)/:path*'],
 };
